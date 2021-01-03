e41766b0633e22f367a94bd37482c9833e77ee23
ExportingImagination <48741484+ExportingImagination@users.noreply.github.com>
2021-01-03 18:39:48 +0100

    Update li.js

--- li.js
+++ li.js
@@ -5,7 +5,17 @@ plist =[
          ['True Confession', 'https://www.dropbox.com/s/u483gzf1y2m456l/True%20Confessions.flac?dl=0'],
          ['Appel Flappen', 'https://www.dropbox.com/s/ouhrlzhzkayee67/Appel%20Flappen.mp3?dl=0'],
          ['Fields Of Sugar', 'https://www.dropbox.com/s/v5k7fppdnknqmwb/Fields%20Of%20Sugar.flac?dl=0'],
-       
+        ['BDSM', 'https://www.dropbox.com/s/04zfbhdyc94bfat/BDSM.mp3?dl=0'],
+          ['Bearem', 'https://www.dropbox.com/s/8x7wsfavro3sfsf/Bearem.flac?dl=0'],
+        
+         ['Something Chill', 'https://www.dropbox.com/s/c5hp4kmktz3l11l/SomethingChill.mp3?dl=0'],
+         ['Deep Gtr', 'https://www.dropbox.com/s/98916672nxnuovc/Deep%20Gtr.mp3?dl=0'],
+        ['Swing Bas 16d', 'https://www.dropbox.com/s/segqlggmuzlm291/swing%20bas%2016d.flac?dl=0'],
+        ['Fields Of Sugar', 'https://www.dropbox.com/s/v5k7fppdnknqmwb/Fields%20Of%20Sugar.flac?dl=0'],
+        ['City Life', 'https://www.dropbox.com/s/cbv4qu7vue1yonb/City%20Life.flac?dl=0'],
+         ['Käppen', 'https://www.dropbox.com/s/szwxwiekn8ixwey/K%C3%A4ppen.mp3?dl=0'],
+         ['Döskalle Vals', 'https://www.dropbox.com/s/hr010djhkjvuxqb/D%C3%B6skalle%20Vals.flac?dl=0'], 
+         ['Arnö Eken', 'https://www.dropbox.com/s/tnl9pd5gnea5li9/Arn%C3%B6%20Eken.mp3?dl=0']
 
         ]
       //['BDSM', 'https://www.dropbox.com/s/04zfbhdyc94bfat/BDSM.mp3?dl=0'],
