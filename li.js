e81df5f293492478980e1ee9c79f6e9efdc8c67a
ExportingImagination <48741484+ExportingImagination@users.noreply.github.com>
2021-01-04 19:55:54 +0100

    Update li.js

--- li.js
+++ li.js
@@ -13,17 +13,17 @@ plist =[
          ['City Lights', 'https://www.dropbox.com/s/cbv4qu7vue1yonb/City%20Life.flac?dl=0'],        
          ['Döskalle Vals', 'https://www.dropbox.com/s/hr010djhkjvuxqb/D%C3%B6skalle%20Vals.flac?dl=0'], 
          ['Arnö Eken', 'https://www.dropbox.com/s/tnl9pd5gnea5li9/Arn%C3%B6%20Eken.mp3?dl=0'],
+         ['Why So Serious?', 'https://www.dropbox.com/s/hf7vs4k4rb7gv5w/why%20so%20serious%20rm.flac?dl=0'],     
+         ['Evol', 'https://www.dropbox.com/s/w17g0mwhlju2ax8/Evol.mp3?dl=0'],        
+         ['Humm', 'https://www.dropbox.com/s/d23ve3b4jee699p/humm.mp3?dl=0'],
+         ['I Dont Have It', 'https://www.dropbox.com/s/aq7x1qi7p1ug4rw/I%20Don%27t%20Have%20It.mp3?dl=0'],
+         ['Laa', 'https://www.dropbox.com/s/vmafncca4tsa0aq/laaa.mp3?dl=0'],      
+         ['Mingel In Berlin', 'https://www.dropbox.com/s/i5hca9xyq0obakv/Mingel%20In%20Berlin.mp3?dl=0'],        
+         ['Trace As In Search', 'https://www.dropbox.com/s/3xy85vy9yj8j3gy/trace%20as%20in%20search.mp3?dl=0'], 
+       
 
         ]
- //['Käppen', 'https://www.dropbox.com/s/szwxwiekn8ixwey/K%C3%A4ppen.mp3?dl=0'],
-      //['BDSM', 'https://www.dropbox.com/s/04zfbhdyc94bfat/BDSM.mp3?dl=0'],
-          //['Bearem', 'https://www.dropbox.com/s/8x7wsfavro3sfsf/Bearem.flac?dl=0'],
+ //['Käppen', 'https://www.dropbox.com/s/ekmak7v4y31v69e/k%C3%A4ppen%20rm2.flac?dl=0'],
+     
         
-      //   ['Something Chill', 'https://www.dropbox.com/s/c5hp4kmktz3l11l/SomethingChill.mp3?dl=0'],
-     //    ['Deep Gtr', 'https://www.dropbox.com/s/98916672nxnuovc/Deep%20Gtr.mp3?dl=0'],
-      //   ['Swing Bas 16d', 'https://www.dropbox.com/s/segqlggmuzlm291/swing%20bas%2016d.flac?dl=0'],
-       //  ['Fields Of Sugar', 'https://www.dropbox.com/s/v5k7fppdnknqmwb/Fields%20Of%20Sugar.flac?dl=0'],
-       //  ['City Life', 'https://www.dropbox.com/s/cbv4qu7vue1yonb/City%20Life.flac?dl=0'],
-       //  ['Käppen', 'https://www.dropbox.com/s/szwxwiekn8ixwey/K%C3%A4ppen.mp3?dl=0'],
-       //  ['Döskalle Vals', 'https://www.dropbox.com/s/hr010djhkjvuxqb/D%C3%B6skalle%20Vals.flac?dl=0'], 
-        // ['Arnö Eken', 'https://www.dropbox.com/s/tnl9pd5gnea5li9/Arn%C3%B6%20Eken.mp3?dl=0']
+     
