4d599f4e86c7fbc3e20e65b9aebb00d61e319dbf
ExportingImagination <48741484+ExportingImagination@users.noreply.github.com>
2021-01-04 15:43:40 +0100

    Update li.js

--- li.js
+++ li.js
@@ -10,7 +10,7 @@ plist =[
          ['Something Chill', 'https://www.dropbox.com/s/c5hp4kmktz3l11l/SomethingChill.mp3?dl=0'],
          ['Deep Gtr', 'https://www.dropbox.com/s/98916672nxnuovc/Deep%20Gtr.mp3?dl=0'],
          ['Swing Bas 16d', 'https://www.dropbox.com/s/segqlggmuzlm291/swing%20bas%2016d.flac?dl=0'],      
-         ['City Life', 'https://www.dropbox.com/s/cbv4qu7vue1yonb/City%20Life.flac?dl=0'],        
+         ['City Lights', 'https://www.dropbox.com/s/cbv4qu7vue1yonb/City%20Life.flac?dl=0'],        
          ['Döskalle Vals', 'https://www.dropbox.com/s/hr010djhkjvuxqb/D%C3%B6skalle%20Vals.flac?dl=0'], 
          ['Arnö Eken', 'https://www.dropbox.com/s/tnl9pd5gnea5li9/Arn%C3%B6%20Eken.mp3?dl=0'],
 
